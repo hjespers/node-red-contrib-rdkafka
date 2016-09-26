76ac892823a22a29f0c941441e4d4e9bb422c33a
Hans Jespersen <hans.jespersen@gmail.com>
2016-09-26 13:58:47 -0700

    Updated README
    added images

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-node-red-contrib-rdkafka
-########################
-
+#node-red-contrib-rdkafka
 
 Node-RED (http://nodered.org) nodes for publish/subscribe messaging using the new Apache Kafka Consumer/Producer API and without requiring direct communications with Zookeeper. Based on the the high performance librdkafka Apache Kafka C/C++ library (https://github.com/edenhill/librdkafka).
 
@@ -22,25 +20,30 @@ Start node-red as normal
 Point your browser to http://localhost:1880
 
 You should see white rdkafka input and output nodes in the pallet on the left side of the screen.
-
-[input png]
-[output png]
+<ul>
+    <li>input <img src="https://github.com/hjespers/node-red-contrib-rdkafka/blob/master/images/rdkafka-in.png"></li>
+    <li>output <img src="https://github.com/hjespers/node-red-contrib-rdkafka/blob/master/images/rdkafka-out.png"></li>
+</ul>
 
 Drag either rdkafka node to the canvas and double click to configure the topic, brokers, clientID and groupID.
 
-[rdkafka config dialog]
+<img src="https://github.com/hjespers/node-red-contrib-rdkafka/blob/master/images/rdkafka-out-config.png">
 
 Click on the pencil icon to the right of the broker selection box to configure a kafka broker connection if one does not already exist.
 
-[broker config dialog]
+<img src="https://github.com/hjespers/node-red-contrib-rdkafka/blob/master/images/rdkafka-broker-config.png">
 
 Publish and subscribe just as you would with the mqtt node with some small differences namely:
-
-topics should not contain "/" or "." characters
-kafka wildcard/regex subscriptions are not yet fully tested
-ensure you have unique Group IDs configured unless you want multiple consumers to be in a Kafka consumer group
+<ul>
+	<li>topics should not contain "/" or "." characters
+	<li>kafka wildcard/regex subscriptions are not yet fully tested
+	<li>ensure you have unique Group IDs configured unless you want multiple consumers to be in a Kafka consumer group
+</ul>
 
 #Author
 
 Hans Jespersen, https://github.com/hjespers
 
+#Feedback and Support
+
+For more information, feedback, or support see https://github.com/hjespers/node-red-contrib-rdkafka/issues
--- rdkafka/rdkafka.js
+++ rdkafka/rdkafka.js
@@ -1,20 +1,17 @@
-
-
+"use strict";
 module.exports = function(RED) {
-    "use strict";
-    var Kafka = require('node-rdkafka'); 
+    var Kafka = require('node-rdkafka');
     var util = require("util");
 
     function KafkaBrokerNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.broker = n.broker;
         this.clientid = n.clientid;
     }
-    RED.nodes.registerType("kafka-broker",KafkaBrokerNode,{
-    });
+    RED.nodes.registerType("kafka-broker", KafkaBrokerNode, {});
 
     function RdKafkaInNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.topic = n.topic;
         this.broker = n.broker;
         this.cgroup = n.cgroup;
@@ -22,121 +19,185 @@ module.exports = function(RED) {
         var node = this;
         var instream;
         if (this.brokerConfig) {
-            node.status({fill:"red",shape:"ring",text:"disconnected"});
-            var consumer = new Kafka.KafkaConsumer({
-                'group.id': this.cgroup,
-                'client.id': this.brokerConfig.clientid,
-                'metadata.broker.list': this.brokerConfig.broker   
-            }, {});
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: "disconnected"
+            });
+            try {
+                var consumer = new Kafka.KafkaConsumer({
+                    'group.id': this.cgroup,
+                    'client.id': this.brokerConfig.clientid,
+                    'metadata.broker.list': this.brokerConfig.broker,
+                    'socket.keepalive.enable': true,
+                    'batch.num.messages': 1000000,
+                    'fetch.wait.max.ms': 100,       //librkafka recommendation for low latency 
+                    'fetch.error.backoff.ms': 100,  //librkafka recommendation for low latency
+                    'queue.buffering.max.ms': 1     //librkafka recommendation for low latency
+                }, {});
+            } catch(e) {
+                console.log(e);
+            }
+
+
+
             if (this.topic) {
-                instream = consumer.getReadStream(this.topic);
+                try {
+                    instream = consumer.getReadStream(this.topic);
+                } catch(e) {
+                    // statements
+                    util.log('[rdkafka] Error creating consumer read stream:' +e);
+                }
+                
                 util.log('[rdkafka] Created input stream on topic = ' + this.topic);
                 //console.log('[rdkafka]  group.id = ' + this.cgroup);
                 //console.log('[rdkafka]  metadata.broker.list = ' + this.brokerConfig.broker);
 
 
                 instream.on('data', function(data) {
-                  //console.log('Got message');
-                  //console.log(data.message.toString());
-                  var msg = {
-                    payload : data.message.toString(),
-                    topic : data.topic,
-                    offset : data.offset,
-                    partition : data.partition,
-                    size : data.size
-                  }
-                  node.send(msg);
+                    //console.log('Got message');
+                    //console.log(data.message.toString());
+                    var msg = {
+                        payload: data.message.toString(),
+                        topic: data.topic,
+                        offset: data.offset,
+                        partition: data.partition,
+                        size: data.size
+                    };
+                    try {
+                        node.send(msg);
+                    } catch(e) {
+                        // statements
+                        util.log('[rdkafka] error sending node message: ' +e);
+                    }
+                    
                 });
 
-                instream.on('error', function (err) {
-                      // Here's where we'll know if something went wrong sending to Kafka
-                      console.error('[rdkafka] Error in our kafka input stream');
-                      console.error(err);
-                });  
+                instream.on('error', function(err) {
+                    // Here's where we'll know if something went wrong sending to Kafka
+                    console.error('[rdkafka] Error in our kafka input stream');
+                    console.error(err);
+                });
 
-                node.status({fill:"green",shape:"dot",text:"connected"});
+                node.status({
+                    fill: "green",
+                    shape: "dot",
+                    text: "connected"
+                });
             } else {
                 this.error('missing input topic');
             }
         } else {
             this.error("missing broker configuration");
-        }   
+        }
         this.on('close', function() {
-            consumer.disconnect(); 
+            consumer.disconnect();
         });
     }
-    RED.nodes.registerType("rdkafka in",RdKafkaInNode);
+    RED.nodes.registerType("rdkafka in", RdKafkaInNode);
 
     function RdKafkaOutNode(n) {
-        RED.nodes.createNode(this,n);
+        RED.nodes.createNode(this, n);
         this.topic = n.topic;
         this.broker = n.broker;
         this.brokerConfig = RED.nodes.getNode(this.broker);
         var node = this;
         var producer, stream;
 
-        if (this.brokerConfig ) {
-            this.status({fill:"red",shape:"ring",text:"disconnected"});
-            producer = new Kafka.Producer({
-               'client.id': this.brokerConfig.clientid,
-               'metadata.broker.list': this.brokerConfig.broker,
-               'compression.codec': 'gzip',
-               'retry.backoff.ms': 200,
-               'message.send.max.retries': 10,
-               'socket.keepalive.enable': true,
-               'queue.buffering.max.messages': 100000,
-               'queue.buffering.max.ms': 100,
-               'batch.num.messages': 1000000,
-               'session.timeout.ms': 5000    
-            })
-            if (this.topic != "" ) {
-                stream = producer.getWriteStream(this.topic);
-                util.log('[rdkafka] Created output stream with topic = ' + this.topic );
+        if (this.brokerConfig) {
+            this.status({
+                fill: "red",
+                shape: "ring",
+                text: "disconnected"
+            });
+            try {
+                producer = new Kafka.Producer({
+                    'client.id': this.brokerConfig.clientid,
+                    'metadata.broker.list': this.brokerConfig.broker,
+                    'compression.codec': 'gzip',
+                    'retry.backoff.ms': 200,
+                    'message.send.max.retries': 10,
+                    'socket.keepalive.enable': true,
+                    'queue.buffering.max.messages': 100000,
+                    'queue.buffering.max.ms': 100,
+                    'batch.num.messages': 1000000
+                });    
+            } catch(e) {
+                // statements
+                console.log(e);
+            }
+
+            if (this.topic != "") {
+                try {
+                    stream = producer.getWriteStream(this.topic);
+                    util.log('[rdkafka] Created output stream with topic = ' + this.topic);
+
+                    stream.on('error', function(err) {
+                        // Here's where we'll know if something went wrong sending to Kafka
+                        util.error('[rdkafka] Error in our kafka stream');
+                        console.error(err);
+                    }); 
+                } catch (e) {
+                    // statements
+                    util.log('[rdkafka] error creating producer writestream: ' + e);
+                }
             }
             // This call returns a new writable stream to our topic 'topic-name'
-            this.status({fill:"green",shape:"dot",text:"connected"});
+            this.status({
+                fill: "green",
+                shape: "dot",
+                text: "connected"
+            });
 
-            this.on("input",function(msg) {
+            this.on("input", function(msg) {
                 if (msg == null || (msg.topic == "" && this.topic == "")) {
                     util.log("[rdkafka] request to send a NULL message or NULL topic on session: " + this.client.ref + " object instance: " + this.client[("_instances")]);
-                } else if (msg != null && msg.topic != "" && this.topic == "" ) {
+                } else if (msg != null && msg.topic != "" && this.topic == "") {
                     // use the topic on the message
-                    var newstream = producer.getWriteStream(msg.topic);
-                    // Writes a message to the stream
-                    var queuedSuccess = newstream.write(msg.payload.toString());
-                    if (queuedSuccess) {
-                      //console.log('[rdkafka] We queued our message using topic from msg.topic!');
-                    } else {
-                      // Note that this only tells us if the stream's queue is full,
-                      // it does NOT tell us if the message got to Kafka!  See below...
-                      util.log('[rdkafka] Too many messages in our queue already');
+                    try {
+                        var newstream = producer.getWriteStream(msg.topic);
+                    } catch (e) {
+                        // statements
+                        util.log('[rdkafka] error creating producer writestream : ' + e);
                     }
 
-                    stream.on('error', function (err) {
-                      // Here's where we'll know if something went wrong sending to Kafka
-                      console.error('[rdkafka] Error in our kafka stream');
-                      console.error(err);
-                    })   
-                } else if (msg != null && this.topic != "" ) {
-                    //console.log('***got here***');
-                    //console.log('topic = ' + this.topic);
-                    //console.log('msg.topic = ' + msg.topic);
-                    //console.log('msg.payload = ' + msg.payload);
+                    // Writes a message to the stream
+                    try {
+                        var queuedSuccess = newstream.write(msg.payload.toString());
+                    } catch(e) {
+                        // statements
+                        util.log('[rdkafka] error writing to producer writestream: ' + e);
+                    }
+
+                    if (queuedSuccess) {
+                        //console.log('[rdkafka] We queued our message using topic from msg.topic!');
+                    } else {
+                        // Note that this only tells us if the stream's queue is full,
+                        // it does NOT tell us if the message got to Kafka!  See below...
+                        util.log('[rdkafka] Too many messages in our queue already');
+                    }
+
+                    newstream.on('error', function(err) {
+                        // Here's where we'll know if something went wrong sending to Kafka
+                        console.error('[rdkafka] Error in our kafka stream');
+                        console.error(err);
+                    });
+                } else if (msg != null && this.topic != "") {
                     // Writes a message to the cached stream
                     var queuedSuccess = stream.write(msg.payload.toString());
                     if (queuedSuccess) {
-                      //console.log('[rdkafka] We queued our message!');
+                        //console.log('[rdkafka] We queued our message!');
                     } else {
-                      // Note that this only tells us if the stream's queue is full,
-                      // it does NOT tell us if the message got to Kafka!  See below...
-                      console.log('[rdkafka]Too many messages in our queue already');
+                        // Note that this only tells us if the stream's queue is full,
+                        // it does NOT tell us if the message got to Kafka!  See below...
+                        util.log('[rdkafka]Too many messages in our queue already');
                     }
-
-                    stream.on('error', function (err) {
-                      // Here's where we'll know if something went wrong sending to Kafka
-                      console.error('[rdkafka] Error in our kafka stream');
-                      console.error(err);
-                    })   
+/*
+                    stream.on('error', function(err) {
+                        // Here's where we'll know if something went wrong sending to Kafka
+                        util.error('[rdkafka] Error in our kafka stream');
+                        console.error(err);
+                    }); */
                 }
             });
         } else {
@@ -146,6 +207,6 @@ module.exports = function(RED) {
             producer.disconnect();
         });
     }
-    RED.nodes.registerType("rdkafka out",RdKafkaOutNode);
+    RED.nodes.registerType("rdkafka out", RdKafkaOutNode);
 
 };
