ad7937436213a068f5e0889aad3fcaa3e2557033
Hans Jespersen <hans.jespersen@gmail.com>
2017-03-28 00:16:13 -0700

    updated consumer to use new node-rdkafka api
    Much better performance for publisher now

--- rdkafka/rdkafka.js
+++ rdkafka/rdkafka.js
@@ -18,92 +18,92 @@ module.exports = function(RED) {
         this.autocommit = n.autocommit;
         this.brokerConfig = RED.nodes.getNode(this.broker);
         var node = this;
-        var instream;
+        var consumer;
         //var topic = this.topic;
-        if (this.brokerConfig) {
+        if (node.brokerConfig !== undefined) {
             node.status({
                 fill: "red",
                 shape: "ring",
                 text: "disconnected"
             });
-            try {
-                var consumer = new Kafka.KafkaConsumer({
-                    'group.id': this.cgroup,
-                    'client.id': this.brokerConfig.clientid,
-                    'metadata.broker.list': this.brokerConfig.broker,
-                    'socket.keepalive.enable': true,
-                    'enable.auto.commit': this.autocommit,
-                    'queue.buffering.max.ms': 1,
-                    'fetch.min.bytes': 1,
-                    'fetch.wait.max.ms': 1,         //librkafka recommendation for low latency 
-                    'fetch.error.backoff.ms': 100   //librkafka recommendation for low latency
-                }, {});
-            } catch(e) {
-                console.log(e);
-            }
+            if (node.topic !== undefined) {  
+                // subscribe to kafka topic (if provided), otherwise print error message
+                try {   
+                    // create node-rdkafka consumer      
+                    consumer = new Kafka.KafkaConsumer({
+                        'group.id': node.cgroup,
+                        'client.id': node.brokerConfig.clientid,
+                        'metadata.broker.list': node.brokerConfig.broker,
+                        'socket.keepalive.enable': true,
+                        'enable.auto.commit': node.autocommit,
+                        'queue.buffering.max.ms': 1,
+                        'fetch.min.bytes': 1,
+                        'fetch.wait.max.ms': 1,         //librkafka recommendation for low latency 
+                        'fetch.error.backoff.ms': 100   //librkafka recommendation for low latency
+                    }, {});
 
-            // subscribe to kafka topic (if provided), otherwise print error message
-            if (this.topic) {
-                try {
-                    //stream api
-                    instream = consumer.getReadStream(this.topic, {
-                        waitInterval: 0
-                    });
+                    // Setup Flowing mode
+                    consumer.connect();
+                
+                    // consumer event handlers
+                    consumer
+                        .on('ready', function() {
+
+                            console.log('[rdkafka] consumer is ready');
+                            node.status({
+                                fill: "green",
+                                shape: "dot",
+                                text: "connected"
+                            });  
+
+                            consumer.subscribe([node.topic]);
+                            consumer.consume();
+                            util.log('[rdkafka] Created consumer subscription on topic = ' + node.topic);
+
+                        })
+                        .on('data', function(data) {
+                            // Output the actual message contents
+                            var msg = {
+                                topic: data.topic,
+                                offset: data.offset,
+                                partition: data.partition,
+                                size: data.size
+                            };
+                            if (data.value) {
+                                msg.payload = data.value.toString();
+                            } else {
+                                msg.payload = ""; //in case of msg with null value
+                                //console.log('data.value was null');
+                            }
+                            if (data.key) {
+                                msg.key = data.key.toString();
+                            }
+                            try {
+                                node.send(msg);
+                            } catch(e) {
+                                // statements
+                                util.log('[rdkafka] error sending node message: ' +e);
+                            }
+                        })
+                        .on('error', function(err) {
+                            // Here's where we'll know if something went wrong consuming from Kafka
+                            console.error('[rdkafka] Error in our kafka consumer');
+                            console.error(err);
+                        });
+
                 } catch(e) {
                     //util.log('[rdkafka] Error creating consumer read stream:' +e);
                     util.log('[rdkafka] Error creating consumer connection:' +e);
                 }
-                
-                util.log('[rdkafka] Created input stream on topic = ' + this.topic);
-
-                node.status({
-                    fill: "green",
-                    shape: "dot",
-                    text: "connected"
-                });                   
-
-                instream.on('data', function(data) {
-                    //console.log('Got message');
-                    //console.log(util.inspect(data));
-                    var msg = {
-                        topic: data.topic,
-                        offset: data.offset,
-                        partition: data.partition,
-                        size: data.size
-                    };
-                    if (data.value) {
-                        msg.payload = data.value.toString();
-                        //console.log(data.value.toString());
-                    } else {
-                        msg.payload = ""; //in case of msg with null value
-                        //console.log('data.value was null');
-                    }
-                    if (data.key) {
-                        msg.key = data.key.toString();
-                    }
-                    try {
-                        node.send(msg);
-                    } catch(e) {
-                        // statements
-                        util.log('[rdkafka] error sending node message: ' +e);
-                    }
-                    
-                });
-
-                instream.on('error', function(err) {
-                    // Here's where we'll know if something went wrong sending to Kafka
-                    console.error('[rdkafka] Error in our kafka input stream');
-                    console.error(err);
-                });
-
             } else {
-                this.error('missing input topic');
+                node.error('missing input topic');
             }
         } else {
-            this.error("missing broker configuration");
+            node.error("missing broker configuration");
         }
-        this.on('close', function() {
-            //consumer.disconnect();
+        node.on('close', function() {
+            consumer.unsubscribe([node.topic]);
+            consumer.disconnect();
         });
     }
     RED.nodes.registerType("rdkafka in", RdKafkaInNode);
@@ -116,7 +116,7 @@ module.exports = function(RED) {
         var node = this;
         var producer, stream;
 
-        if (this.brokerConfig) {
+        if (node.brokerConfig !== undefined) {
             this.status({
                 fill: "red",
                 shape: "ring",
@@ -124,8 +124,8 @@ module.exports = function(RED) {
             });
             try {
                 producer = new Kafka.Producer({
-                    'client.id': this.brokerConfig.clientid,
-                    'metadata.broker.list': this.brokerConfig.broker,
+                    'client.id': node.brokerConfig.clientid,
+                    'metadata.broker.list': node.brokerConfig.broker,
                     //'compression.codec': 'gzip',
                     'retry.backoff.ms': 200,
                     'message.send.max.retries': 15,
@@ -139,10 +139,10 @@ module.exports = function(RED) {
             }
 
             // if kafka topic is specified it takes precidence over msg.topic so just create a output stream once and reuse it
-            if (this.topic != "") {
+            if (this.topic !== "") {
                 try {
-                    stream = producer.getWriteStream(this.topic);
-                    util.log('[rdkafka] Created output stream with topic = ' + this.topic);
+                    stream = producer.getWriteStream(node.topic);
+                    util.log('[rdkafka] Created output stream with topic = ' + node.topic);
 
                     stream.on('error', function(err) {
                         // Here's where we'll know if something went wrong sending to Kafka
@@ -162,19 +162,19 @@ module.exports = function(RED) {
 
             this.on("input", function(msg) {
                 //handle different payload types including JSON object
-                var message;
+                var message, newstream, queuedSuccess;
                 if( typeof msg.payload === 'object') {
                     message = JSON.stringify(msg.payload);
                 } else {
                     message = msg.payload.toString();
                 }
 
-                if (msg == null || (msg.topic == "" && this.topic == "")) {
-                    util.log("[rdkafka] request to send a NULL message or NULL topic on session: " + this.client.ref + " object instance: " + this.client[("_instances")]);
-                } else if (msg != null && msg.topic != "" && this.topic == "") {
+                if (msg === null || (msg.topic === "" && node.topic === "")) {
+                    util.log("[rdkafka] request to send a NULL message or NULL topic");
+                } else if (msg !== null && msg.topic !== "" && node.topic === "") {
                     // use the topic specified on the message since one is not configured 
                     try {
-                        var newstream = producer.getWriteStream(msg.topic);
+                        newstream = producer.getWriteStream(msg.topic);
                     } catch (e) {
                         // statements
                         util.log('[rdkafka] error creating producer write stream : ' + e);
@@ -182,7 +182,7 @@ module.exports = function(RED) {
 
                     // Writes a message to the stream
                     try {
-                        var queuedSuccess = newstream.write(message);
+                        queuedSuccess = newstream.write(message);
                     } catch(e) {
                         util.log('[rdkafka] error writing to producer writestream: ' + e);
                     }
@@ -200,9 +200,9 @@ module.exports = function(RED) {
                         console.error('[rdkafka] Error in our kafka stream');
                         console.error(err);
                     });
-                } else if (msg != null && this.topic != "") {
+                } else if (msg !== null && node.topic !== "") {
                     // Writes a message to the cached stream
-                    var queuedSuccess = stream.write(message);
+                    queuedSuccess = stream.write(message);
                     if (queuedSuccess) {
                         //console.log('[rdkafka] We queued our message!');
                     } else {
